4444bb1f795d1e746c4cb15ce0836e74e17e294b
Thomas Rohn <trohn54@gmail.com>
2021-04-01 22:17:26 -0500

    File I/O

--- BanasTutorial/Program.cs
+++ BanasTutorial/Program.cs
@@ -7,6 +7,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using System.Numerics;
 using System.Globalization;
+using System.IO;
 
 namespace BanasTutorial
 {
@@ -14,7 +15,7 @@ namespace BanasTutorial
     {
         static void Main(string[] args)
         {
-            Part16(args);
+            Part17(args);
         }        
 
         private static void SayHello() //"Keep functions to about 10 lines of code per function
@@ -1188,6 +1189,95 @@ namespace BanasTutorial
             {
                 Console.WriteLine(i); 
             }
+        }
+
+        private static void Part17(string[] args)
+        {
+            #region directories
+            DirectoryInfo currDirectory = new DirectoryInfo("."); //current directory
+            DirectoryInfo dereksDir = new DirectoryInfo(@"C:\Users\toejo"); //different directory
+            Console.WriteLine(dereksDir.FullName);
+            Console.WriteLine(dereksDir.Name);
+            Console.WriteLine(dereksDir.Parent);
+            Console.WriteLine(dereksDir.Attributes);
+            Console.WriteLine(dereksDir.CreationTime);
+
+            DirectoryInfo dataDir = new DirectoryInfo(@"C:\Users\toejo\c#data"); //create a directory
+            //Directory.Delete(@"C:\Users\toejo\c#data");
+            string[] customers =
+            {
+                "Bob Smith", "Sally Smith", "Robert Smith"
+            };
+
+            string textFilePath = @"C:\Users\toejo\c#data\testfile1.txt";
+
+            File.WriteAllLines(textFilePath, customers);
+
+            foreach(string cust in File.ReadAllLines(textFilePath))
+            {
+                Console.WriteLine(cust);
+            }
+
+            DirectoryInfo myDataDir = new DirectoryInfo(@"C:\Users\toejo\c#data");
+            FileInfo[] textFiles = myDataDir.GetFiles("*.txt", SearchOption.AllDirectories);
+
+            Console.WriteLine("Matches {0}", textFiles.Length);
+            foreach(FileInfo file in textFiles)
+            {
+                Console.WriteLine(file.Name);
+                Console.WriteLine(file.Length);
+            }
+            #endregion
+            #region filestream
+            //for reading and writing bytes
+            string textFilePath2 = @"C:\Users\toejo\c#data\testfile2.txt";
+
+            FileStream fs = File.Open(textFilePath2, FileMode.Create);
+            string randString = "This is a random string";
+            byte[] rsByteArray = Encoding.Default.GetBytes(randString);
+
+            fs.Write(rsByteArray, 0, rsByteArray.Length);
+            fs.Position = 0;
+            byte[] fileByteArray = new byte[rsByteArray.Length];
+            for(int i = 0; i < rsByteArray.Length; i++)
+            {
+                fileByteArray[i] = (byte)fs.ReadByte();
+            }
+            Console.WriteLine(Encoding.Default.GetString(fileByteArray));
+            fs.Close();
+            #endregion
+            #region streamWriter/streamReader
+            string textFilePath3 = @"C:\Users\toejo\c#data\testfile3.txt";
+            StreamWriter sw = File.CreateText(textFilePath3);
+            sw.Write("This is a random ");
+            sw.WriteLine("sentence");
+            sw.WriteLine("and another sentence");
+
+            sw.Close();
+            StreamReader sr = File.OpenText(textFilePath3);
+            Console.WriteLine("Peek : {0}", Convert.ToChar(sr.Peek())); //return value but it stays at the start of the file
+            Console.WriteLine("1st string : {0}", Convert.ToChar(sr.ReadLine()));
+            Console.WriteLine("Everything: {0}", sr.ReadToEnd());
+            sr.Close();
+            #endregion
+            #region binaryWriter/binaryReader
+            //read and write data types
+            string textFilePath4 = @"C:\Users\toejo\c#data\testfile4.dat";
+            FileInfo datFile = new FileInfo(textFilePath4);
+            BinaryWriter bw = new BinaryWriter(datFile.OpenWrite());
+            string randText = "Random Text";
+            int myAge = 23;
+            double height = 6.25;
+            bw.Write(randText);
+            bw.Write(myAge);
+            bw.Write(height);
+            bw.Close();
+            BinaryReader br = new BinaryReader(datFile.OpenRead());
+            Console.WriteLine(br.ReadString());
+            Console.WriteLine(br.ReadInt32());
+            Console.WriteLine(br.ReadDouble());
+            br.Close();
+            #endregion
         }
     }
 
